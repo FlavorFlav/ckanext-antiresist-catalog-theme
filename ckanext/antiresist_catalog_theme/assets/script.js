f703b63bea8e21b56a2cacdc7c1b3276a1ba96c5
Flavio Ackermann <flavio.ackermann@unibas.ch>
2024-02-13 15:23:01 +0100

    Initial commit

---
+++ ckanext/antiresist_catalog_theme/assets/script.js
@@ -0,0 +1,10 @@
+ckan.module("antiresist_catalog_theme-module", function ($, _) {
+  "use strict";
+  return {
+    options: {
+      debug: false,
+    },
+
+    initialize: function () {},
+  };
+});
